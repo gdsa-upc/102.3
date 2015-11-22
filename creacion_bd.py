a14c506ad37359c032437174c1f94edbf8ca50b4
mateomv92 <mateomv92@gmail.com>
2015-11-22 13:15:12 +0100

    Actualización

---
+++ creacion_bd.py
@@ -0,0 +1,10 @@
+import os 
+def creacion_bd(tipo,indir,outdir):
+    images = os.listdir(indir) 
+    outfile = open(outdir+'/DB_Annotation.txt','w')  
+    for file in images:
+        outfile.write(file[0:-4]+"\n") 
+    outfile.close()
+ruta = os.path.dirname(os.path.abspath("__file__"))
+creacion_bd("train",ruta+'/TerrassaBuildings900/TerrassaBuildings900/train/images',ruta+'/TerrassaBuildings900/TerrassaBuildings900/train')
+creacion_bd("val",ruta+'/TerrassaBuildings900/TerrassaBuildings900/val/images',ruta+'/TerrassaBuildings900/TerrassaBuildings900/val')
