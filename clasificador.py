c252ec9418cf0dbc798fcd0d60c55d3b09941a16
sadek4 <oscar.atiki@gmail.com>
2015-11-23 11:24:53 +0100

    Clasificador revisat
    Us pujo el classificador acabat a falta de revisió

--- clasificador.py
+++ clasificador.py
@@ -1,32 +1,35 @@
-import open
 import pickle
+import random
 
-labels = ['catedral','ajuntament','desconegut']
+labels = ['catedral','mercat_independencia','mnactec','masia_freixa','castell_cartoixa','societat_general','estacio_nord','dona_treballadora','escola_enginyeria','farmacia_albinyana','teatre_principal','ajuntament','desconegut']
 line=1
 
-def classify (outdir):
+def classify (valors_id, outdir):
     f=open(outdir,"w")
-    pkl_file = open('Val_diccionari.pkl', 'rb')
-    mydict = pickle.load('Val_diccionari')
-    pkl_file.close()
+    val_id = open(valors_id, 'rb')
+    mydict = pickle.load(val_id)
+    val_id.close()
     print mydict
-
     ides=mydict.keys()
 
     for ids in ides:
-    for line in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]:
-        f.write(ids + ":" + labels[line] + "\n")
-        if line ==15:
-            line=1
-            else     line = line+1
+            for line in len(labels):
+                f.write(ids + ":" + labels[random.choice(15)] + '\n')
+                if line ==15:
+                    line=1
+                else : 
+                    line = line+1
+    f.close()
+    print f
+        
             
         
 
     
 
 
-for line in lines:
-    diccionari[line.split(" ")[0]]=line.split(" ")[1]
-    print diccionari
+#for line in lines:
+#  diccionari[line.split(" ")[0]]=line.split(" ")[1]
+ #   print diccionari
     
     
