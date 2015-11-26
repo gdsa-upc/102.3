5377b582c1096d67972f0486fc5f676ea5631ba3
mateomv92 <mateomv92@gmail.com>
2015-11-26 15:19:48 +0100

    identación funcion parametros
    correccion de errores de la identación. Falta lo de ClassID.

--- Parametres.py
+++ Parametres.py
@@ -6,41 +6,37 @@ import pandas as p
 
 def parametres():
     
-params={}
+    params = {}
 
-#inputs
-params['arrel_entrada']='C:/Users/Gerard/Documents/Uni/GDSA_projecte/carpeta_in'
-params['bd_imatges']='Terrassabuildings900'
-
-#output
-params['arrel_sortida']='C:/Users/Gerard/Documents/Uni/GDSA_projecte/carpeta_out'
-
-#params
-params['mida_descriptor']=10
-identificacions=p.read_csv(os.path.join('arrel_entrada','db_imatges','train','annotation.txt'),sep='\t',header=0)
-#identificacions conté tot el fitxer annotation.txt (classificat en id's i noms d'edificis)
-params['Possibles_edificis']=n.unique(identificacions[ClassID])
-#possibles_edificis conté els noms de les 13 possibles categories (12 edificis i desconegut)
-
-crea_dirs(params)
-
-return params
-
-#crea_nou_dir: si el directori no exixteix, el crea
+    #inputs
+    params['arrel_entrada']='C:/Users/Gerard/Documents/Uni/GDSA_projecte/carpeta_in'
+    params['bd_imatges']='Terrassabuildings900'
+    
+    #output
+    params['arrel_sortida']='C:/Users/Gerard/Documents/Uni/GDSA_projecte/carpeta_out'
+    
+    #params
+    params['mida_descriptor']=10
+    identificacions = p.read_csv(os.path.join('arrel_entrada','db_imatges','train','annotation.txt'),sep='\t',header=0)
+    #identificacions conté tot el fitxer annotation.txt (classificat en id's i noms d'edificis)
+    params['Possibles_edificis'] = n.unique(identificacions[ClassID])
+    #possibles_edificis conté els noms de les 13 possibles categories (12 edificis i desconegut)
+    #crea_dirs(params)
+    
+    return params
+    
+    #crea_nou_dir: si el directori no exixteix, el crea
 def crea_nou_dir(directori):
     if not os.path.isdir(directori):
         os.makedirs(directori)
-
-#crea_dirs: crea tots els directoris que es necessiten i no existeixen cridant 'crea_nou_dir'
+    
+    #crea_dirs: crea tots els directoris que es necessiten i no existeixen cridant 'crea_nou_dir'
 def crea_dirs(params):
-
     dir_out = os.path(params['arrel_sortida'])
     crea_nou_dir(dir_out)
     crea_nou_dir(os.path.join(dir_out,'llista_imgs'))
     crea_nou_dir(os.path.join(dir_out,'features'))
     crea_nou_dir(os.path.join(dir_out,'rankings'))
     crea_nou_dir(os.path.join(dir_out,'classificacions'))
-
-if __name__ == "__main__":
-
-    Parametres_locals = parametres()
+    if __name__ == "__main__":
+        Parametres_locals = parametres()
