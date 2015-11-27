01c7b9ef629e65160d0006cb0b2e652825b1638d
gerardrosell <gerard.rosell.cardus@gmail.com>
2015-11-27 16:27:12 +0100

    Funció paràmetres
    Recordeu a canviar i crear tots els directoris per cada un dels PC's

--- Parametres.py
+++ Parametres.py
@@ -6,25 +6,28 @@ import pandas as p
 
 def parametres():
     
-params={}
+    params = { }
 
 #inputs
-params['arrel_entrada']='C:/Users/Gerard/Documents/Uni/GDSA_projecte/carpeta_in'
-params['bd_imatges']='Terrassabuildings900'
+    params['arrel_entrada']='C:/Users/Gerard/Documents/Uni/GDSA_projecte/carpeta_in'
+    params['bd_imatges']='Terrassabuildings900'
 
 #output
-params['arrel_sortida']='C:/Users/Gerard/Documents/Uni/GDSA_projecte/carpeta_out'
+    params['arrel_sortida']='C:/Users/Gerard/Documents/Uni/GDSA_projecte/carpeta_out'
+    crea_dirs(params)
 
 #params
-params['mida_descriptor']=10
-identificacions=p.read_csv(os.path.join('arrel_entrada','db_imatges','train','annotation.txt'),sep='\t',header=0)
+    params['mida_descriptor']=10
+    identificacions=p.read_csv('C:/Users/Gerard/Documents/Uni/GDSA_projecte/carpeta_in/Terrassabuldings900/train/annotation.txt', sep='\t', header = 0)
+#Copieu el path sencer igual que he fet jo perquè la funció read_csv posa les / al revés i no reconeix el path
+
 #identificacions conté tot el fitxer annotation.txt (classificat en id's i noms d'edificis)
-params['Possibles_edificis']=n.unique(identificacions[ClassID])
+    params['Possibles_edificis']=n.unique(identificacions['ClassID'])
 #possibles_edificis conté els noms de les 13 possibles categories (12 edificis i desconegut)
 
-crea_dirs(params)
+    
 
-return params
+    return params
 
 #crea_nou_dir: si el directori no exixteix, el crea
 def crea_nou_dir(directori):
@@ -34,7 +37,7 @@ def crea_nou_dir(directori):
 #crea_dirs: crea tots els directoris que es necessiten i no existeixen cridant 'crea_nou_dir'
 def crea_dirs(params):
 
-    dir_out = os.path(params['arrel_sortida'])
+    dir_out = os.path.join(params['arrel_sortida'])
     crea_nou_dir(dir_out)
     crea_nou_dir(os.path.join(dir_out,'llista_imgs'))
     crea_nou_dir(os.path.join(dir_out,'features'))
@@ -43,4 +46,4 @@ def crea_dirs(params):
 
 if __name__ == "__main__":
 
-    Parametres_locals = parametres()
+#print parametres()
