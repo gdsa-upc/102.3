2dc6669bc3c4a0f1aa519ebb93a08425e53a5094
mateomv92 <mateomv92@gmail.com>
2015-11-28 12:38:10 +0100

    Actualización get_local_features
    He puesto la función resize dentro de esta función por lo que de dos
    funciones pasamos a solo una. También hay un plot para que podais
    comparar los resultados del reescalado (salen algo distorsionadas las
    imagenes pero se sigue viendo que es la imagen original)

--- get_local_features.py
+++ get_local_features.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 import cv2
-import resize_image
+import matplotlib.pyplot as plt
 def get_local_features(ruta):
     img = cv2.imread(ruta) # Leemos la imagen que pasamos como parámetro
-    rimg = resize_image(img)
+    r = 400.0 / img.shape[1] # El atributo shape retorna las dimensiones del array. Si el array es (m,n) i.shape[1] retorna n
+    dim = (400, int(img.shape[0] * r)) # 
+    rimg = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+    plt.imshow(img),plt.show()
+    plt.imshow(rimg),plt.show()
     sift = cv2.SIFT() # calcula la lista de descriptores de la imagen
-    keypoints, descriptores = sift.detectAndCompute(rimg,None) # obtinc els key points amb els seus descriptors
-    #cambiar rimg por image cuando queramos hacerl ocon las imagenes completas
+    keypoints, descriptores = sift.detectAndCompute(rimg,None)
     return keypoints,descriptores
 
     
