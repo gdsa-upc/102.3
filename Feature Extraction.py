20d060d941b5f5efd5141d916e5dc3d917629fec
gerardrosell <gerard.rosell.cardus@gmail.com>
2015-11-29 13:29:06 +0100

    get_local_features+Feature Extraction
    Veureu que he eliminat el notebook1023 pero en realitat li he canviat el
    nom (get_local...) i l'he modificat

--- Feature Extraction.py
+++ Feature Extraction.py
@@ -1,22 +1,23 @@
 # -*- coding: utf-8 -*-
+from get_local_features_orb import get_local_features_orb
+#from get_local_features_shift import get_local_features_shift
+from Parametres import parametres
 import numpy as np
 import pickle
-def extraccio_caract(str):
-    features=np.random.random_integers(0,100,(1,100))
-    return features;
-#def Feature_extraction(dir_imatges, IDs_file, dir_out):
-def Feature_extraction(IDs_file, dir_out):
+
+def Feature_extraction():
+    
     #comença la inserció al diccionari
-    images=open(IDs_file, "r") #obre l'arxiu on hi ha les id's en mode lectura
+    images=open('Train_ID.txt', "r") #obre l'arxiu on hi ha les id's en mode lectura
 
     diccionari={} #crea un diccionari buit
     #per cada vegada que pugui llegir una línia (un id) del fitxer executa el loop sencer
     for img in images:
         i=img.rstrip('\n')
         #guarda a la posició 'img' del diccionari (una id que ha llegit) un vector que retorna la funcio que extreu característiques
-        diccionari.update({i: extraccio_caract(img)}) 
+        diccionari.update({i: get_local_features_orb(img)}) 
     images.close()
-    with open(dir_out+'/diccionari.pickle', 'wb') as dic:
+    with open('/diccionari.pickle', 'wb') as dic:
       pickle.dump(diccionari, dic)
     return;
     
