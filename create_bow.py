4b682747466323071e6d3c86751305cf95b60f9f
ariadnana <ariadnaxm@gmail.com>
2015-11-29 21:22:29 +0100

    Notebooks borrats, creats assignment i create_bow i arreglat codebook

---
+++ create_bow.py
@@ -0,0 +1,13 @@
+import math
+import numpy
+def build_bow(code,k):
+    a=0
+    L2=0
+    bow=[]
+    for a in a<k:
+        c=code.count(a)
+        bow.append(c)
+        L2=L2+(c*c)
+    s=math.sqrt(s)
+    c=c/s
+    return c
