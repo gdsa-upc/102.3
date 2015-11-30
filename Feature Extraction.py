5d88adb35c61f8f6d74ba6511539a9b868635e76
mateomv92 <mateomv92@gmail.com>
2015-11-30 13:57:11 +0100

    cambios funciones
    orden train, mejora de la funcion

--- Feature Extraction.py
+++ Feature Extraction.py
@@ -4,27 +4,28 @@ from Parametres import parametres
 from assignment import get_assignments
 from get_local_features_orb import get_local_features_orb
 from create_bow import build_bow
-#from get_local_features_shift import get_local_features_shift
+#from get_local_features_sift import get_local_features_sift
 from build_database import build_database
 import numpy as np
+import os 
 
 def Feature_extraction():
     ####Extract Local Features
     params=parametres()
-    build_database('/'.join([params['arrel_entrada'],params['bd_imatges'],'train','images']), '/'.join([params['arrel_sortida'],'llista_imgs']))
-    images=open('/'.join([params['arrel_sortida'],'llista_imgs','ID.txt']), "r") #obre l'arxiu on hi ha les id's en mode lectura
-    
+    #build_database('/'.join([params['arrel_entrada'],params['bd_imatges'],'train','images']), '/'.join([params['arrel_sortida'],'llista_imgs']))
+    #images=open('/'.join([params['arrel_sortida'],'llista_imgs','ID.txt']), "r") #obre l'arxiu on hi ha les id's en mode lectura
+    images = os.listdir('/'.join([params['arrel_sortida'],'llista_imgs','ID.txt']))
     descriptors=[]
     #per cada vegada que pugui llegir una línia (un id) del fitxer executa el loop sencer
     for img in images:
         k, des=get_local_features_orb(img)
         descriptors.append(des) 
-    images.close()
+    #images.close()
     #print descriptors; es para en aquest punt i no agafa bé els valors assignant None, None, ...
     
     ####Train CodeBook
     clusters=4
-    code_book, dist, centres=train_codebook(clusters, descriptors)
+    code_book, dist, centres=train_codebook(descriptors,clusters)
     
     ####Compute Assignaments
     images=open('ID.txt', "r") #obre l'arxiu on hi ha les id's en mode lectura
--- codebook.py
+++ codebook.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
-import cv2
-from scipy.cluster.vq import  whiten
+#import cv2
+from scipy.cluster.vq import  whiten,kmeans
 def train_codebook(desc,k):
-    whitened = whiten(desc)
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 1, 1.0) 
-    a, b, c= cv2.kmeans(whitened,k,criteria,1,cv2.KMEANS_PP_CENTERS)
+    whitened = whiten(desc[0:5])
+    #criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 1, 1.0) 
+    #a, b, c= cv2.kmeans(whitened,k,criteria,1,cv2.KMEANS_PP_CENTERS)
     #prints de comprovació, esborrar-los
-    print a
-    print b
-    print c
-    return a,b,c
+    
+    return kmeans(whitened, k)
