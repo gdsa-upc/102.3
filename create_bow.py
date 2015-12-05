d82defaff9407d9480c13f464165a9361b4590b7
mateomv92 <mateomv92@gmail.com>
2015-12-05 13:08:17 +0100

    Create bow corrección
    Cambio de la función para no usar el bincount

--- create_bow.py
+++ create_bow.py
@@ -1,7 +1,9 @@
-import numpy as np
 from sklearn import preprocessing
-def build_bow(code):
-    v=np.bincount(code)
-    print v
-    vn=preprocessing.normalize(v)
+def build_bow(code,nc):
+    bow=[0]*nc
+    for x in range(0,len(bow)):
+        for i in code:
+            if i==x:
+                bow[x]+=1
+    vn=preprocessing.normalize(bow)[0]
     return vn
