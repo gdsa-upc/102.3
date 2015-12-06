32d480585ccc67e37a9bd2b91cdbfd8e5dba3bd1
mateomv92 <mateomv92@gmail.com>
2015-12-06 19:42:21 +0100

    Revert "estos son los archivos que he modificado"
    This reverts commit 30c4db5733fb293af544299980052b09e8bce0b0.

--- carpeta pruebas/assignment.py
+++
@@ -1,8 +0,0 @@
-from numpy import array
-from scipy.cluster.vq import vq
-from scipy.cluster.vq import whiten
-def get_assignments(desc, codebook):
-    d=whiten(desc)
-    #code,dist=vq(d,codebook)
-    code=codebook.predict(d)
-    return code
