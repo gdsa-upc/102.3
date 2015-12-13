607c96d7ef24187c1d764b394633d15ec6699d53
mateomv92 <mateomv92@gmail.com>
2015-12-13 16:38:18 +0100

    Update prueba_classify.py

--- prueba_classify.py
+++ prueba_classify.py
@@ -7,6 +7,6 @@ def classify(features, model):
     classifier= pickle.load( open(params['arrel_sortida']+'/model_classifier.pickle', 'r') )#cargamos el clasificador entrenado
     outfile = open(params['arrel_sortida']+'/classification.txt', 'wb')#creamos directorio de salida
     print bow_val
-    for im_id, im_bow in bow_val.iteritems():
+    for im_id, im_bow in bow_val.items():
         outfile.write(str(im_id) + "\t" + str(classifier.predict(im_bow)[0]) + "\n"  ) # escribimos la id de la imagen con la predicción del clasificador
     outfile.close()
