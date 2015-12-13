575ab392a77eb0fa9862d7fdce51739c2b0effde
gerardrosell <gerard.rosell.cardus@gmail.com>
2015-12-13 17:49:58 +0100

    train_classifier arreclat
    está funcionant

--- train_classifier.py
+++ train_classifier.py
@@ -16,28 +16,32 @@ def train_classifier():
         gtt_dic[line[0:a]]=line[a+1:len(line)-1]
     f.close()
     
-    classes = []#inicialitzem el vector classes.
-    for classe in gtt_dic.values(): #l'omplim només amb un valor/classe--després miraré com fer-ho amb el pes.
-        if classe not in classes:
-            classes.append(classe)
+    #classes = []#inicialitzem el vector classes.
+    #for classe in gtt_dic.values(): #l'omplim només amb un valor/classe--després miraré com fer-ho amb el pes.
+    #    if classe not in classes:
+    #        classes.append(classe)
     
-    #claus = mydict.keys()
     #BoW = mydict.values()
     
-    valors = {} #diccionari classe:BoW
-    for i in gtt_dic.keys(): #l'omplim amb els ids i classes de gtt i els bow de mydict
+    valors = [] #creació de la llista on guardarem (en el mateix ordre) a quin edifici pertany cada BoW
+    for i in gtt_dic.keys(): #recorrent el vector groundtruth
         for k in mydict.keys():
             if i == k:
-                valors[i]= gtt_dic[k]#aqui estas guardant en un diccionari com a clau el id i com a value el mateix id, no crec k sigui el k vols fer 
-    #print gtt_dic
-                                  
-    model = svm.SVC()   
-    model.fit(valors, classes)
+                valors.append(gtt_dic[k]) #assignem a mida que trobem coincidencies el nom de l'edifici al final de la llista
+    
+    #El resultat ve a ser:
+    #mydict.values()=[BoW1],[BoW2],...,[BoW450]
+    #valors= edifici1,edifici2,...,edifici450
+    #i queden lligats per l'ordre
+                      
+    model = svm.SVC()  
+    model.fit(mydict.values(),valors)
         
     #SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
     #decision_function_shape=None, degree=3, gamma='auto', kernel='rbf',
     #max_iter=-1, probability=False, random_state=None, shrinking=True,
     #tol=0.001, verbose=False) 
+    
     
     with open(params['arrel_sortida']+'/model_classifier.pickle', 'wb') as save_model:
         pickle.dump(model,save_model)
