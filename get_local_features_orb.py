c87d7f1cc232164ee64544dbd28d33ab88a61122
sadek4 <oscar.atiki@gmail.com>
2015-12-14 04:08:09 +0100

    Per si us interessa
    He estat intentant utilitzar el rootsift x veure si dona millors
    resultats però no m'ha sortit.. per si voleu seguir inentantho aquí us
    ho deixo..

--- get_local_features_orb.py
+++ get_local_features_orb.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from Parametres import parametres
+from get_feat_root import RootSIFT
 import cv2
 #from matplotlib import pyplot as plt
 
@@ -11,15 +12,15 @@ def get_local_features_orb(imatge_entrada):
         img=cv2.imread('/'.join([params['arrel_entrada'],params['bd_imatges'],'val','images',imatge_entrada]),0)
     #fi arreglo provisional
 # nombre màxim de kp a extreure
-    n=20
+    #n=500
 # Iniciem el detector ORB
-    orb = cv2.ORB(nfeatures=n)
+    rs = RootSIFT()
     
 #kp = orb.detect(img,None)  +  kp, des = orb.compute(img, kp)
     r = 400.0 / img.shape[1]
     dim = (400, int(img.shape[0] * r))
     rimg = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-    kp , des= orb.detectAndCompute(rimg,None)
+    kp , des= rs.compute(rimg,None)
 
 # dibuixa els descriptors sobre la imatge amb representació segons la importància
     #img_kp = cv2.drawKeypoints(img,kp,flags=4)
