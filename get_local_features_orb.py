157bd62e53369c504140036409280fa405bf2f7c
sadek4 <oscar.atiki@gmail.com>
2015-12-14 13:09:57 +0100

    Revert "Per si us interessa"
    This reverts commit c87d7f1cc232164ee64544dbd28d33ab88a61122.

--- get_local_features_orb.py
+++ get_local_features_orb.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from Parametres import parametres
-from get_feat_root import RootSIFT
 import cv2
 #from matplotlib import pyplot as plt
 
@@ -12,15 +11,15 @@ def get_local_features_orb(imatge_entrada):
         img=cv2.imread('/'.join([params['arrel_entrada'],params['bd_imatges'],'val','images',imatge_entrada]),0)
     #fi arreglo provisional
 # nombre màxim de kp a extreure
-    #n=500
+    n=20
 # Iniciem el detector ORB
-    rs = RootSIFT()
+    orb = cv2.ORB(nfeatures=n)
     
 #kp = orb.detect(img,None)  +  kp, des = orb.compute(img, kp)
     r = 400.0 / img.shape[1]
     dim = (400, int(img.shape[0] * r))
     rimg = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-    kp , des= rs.compute(rimg,None)
+    kp , des= orb.detectAndCompute(rimg,None)
 
 # dibuixa els descriptors sobre la imatge amb representació segons la importància
     #img_kp = cv2.drawKeypoints(img,kp,flags=4)
